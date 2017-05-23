f7f697ff7533c006e4e8b8483f2ac312a5325a67
Keith Twombley <daoist@keithtwombley.com>
2017-05-23 15:43:16 -0500

    Automatic Update

--- go.sh
+++ go.sh
@@ -1,3 +1,6 @@
 wget https://patribotics.blog/sitemap.xml --output-document sitemap.xml -nv
 cat sitemap.xml | egrep -o "https?://[^<]+" | wget -nv --mirror --wait .5 --random-wait --no-parent --domains patribotics.blog,patribotics.files.wordpress.com,patribotics.wordpress.com --reject-regex '.*\?.*|/feed/$|/amp/$|.*\"https:.*|.*/category/.*|.*/tag/.*|.jpeg$|.jpg$|.png$|.gif$' -R jpeg,jpg,png,gif -i -
-
+git add sitemap.xml
+git add patribotics.blog/*
+git add patribotics.files.wordpress.com/*
+git commit -m "Automatic Update"

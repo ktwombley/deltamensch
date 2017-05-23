77c29be182b23240ad0cddbe9632a5a8c104cc0d
Keith Twombley <daoist@keithtwombley.com>
2017-05-23 17:14:47 -0500

    now tries to make a diffable copy of each page

---
+++ diffable.sed
@@ -0,0 +1,9 @@
+s|(<script src='https://r-login.wordpress.com/remote-login.php).*(</script>)|\1##Scriptpath##\2|g
+s|crt-[^'"]+|crt-##DivID##|g
+s|('like-post-)[^']+|\1##ID##|g
+s|(value=")\d+|\1##Number##|g
+s|Join [\d,]+ other followers|Join ##Followers## other followers|g
+s|(_stq.push\(\['extra', \{'crypt':).*('\}\]\);)|\1##Crypt##\2q|g
+s|^.*generated.*seconds.*$|##GeneratedSeconds##|g
+s|^.*batcache.*seconds.*$|##Batcache##|g
+
--- go.sh
+++ go.sh
@@ -1,7 +1,7 @@
 wget https://patribotics.blog/sitemap.xml --output-document sitemap.xml -nv
 cat sitemap.xml | egrep -o "https?://[^<]+" | wget -nv --mirror --wait .5 --random-wait --no-parent --domains patribotics.blog,patribotics.files.wordpress.com,patribotics.wordpress.com --reject-regex '.*\?.*|/feed/$|/amp/$|.*\"https:.*|.*/category/.*|.*/tag/.*|.jpeg$|.jpg$|.png$|.gif$' -R jpeg,jpg,png,gif -i -
+find patribotics.blog/ -type f -not -name "*.diffable" -exec sh -c 'mkdir -p `dirname ./diffable/{}`; sed -r -f diffable.sed {} > ./diffable/{}.diffable' \;
 git add sitemap.xml
 git add patribotics.blog/*
-git add patribotics.files.wordpress.com/*
 git commit -m "Automatic Update `date -u`"
 git push -u origin master

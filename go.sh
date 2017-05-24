6719b4b5abe8307414978639aefa8bcb61afcff1
Keith Twombley <daoist@keithtwombley.com>
2017-05-23 19:55:22 -0500

    added a shebang line to go.sh. what have I become?

--- go.sh
+++ go.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 wget https://patribotics.blog/sitemap.xml --output-document sitemap.xml -nv
 cat sitemap.xml | egrep -o "https?://[^<]+" | wget -nv --mirror --wait .5 --random-wait --no-parent --domains patribotics.blog,patribotics.files.wordpress.com,patribotics.wordpress.com --reject-regex '.*\?.*|/feed/$|/amp/$|.*\"https:.*|.*/category/.*|.*/tag/.*|.jpeg$|.jpg$|.png$|.gif$' -R jpeg,jpg,png,gif -i -
 find patribotics.blog/ -type f -not -name "*.diffable" -exec sh -c 'mkdir -p `dirname ./diffable/{}`; sed -r -f diffable.sed {} > ./diffable/{}.diffable' \;
